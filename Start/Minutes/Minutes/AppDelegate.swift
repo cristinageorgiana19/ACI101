4c63839bd3e3bad2e6f67041e55aa92668774d30
Cristina Marin <cristina.marin@softvision.ro>
2020-02-17 15:01:05 +0200

    added frameworks and test target

--- Start/Minutes/Minutes/AppDelegate.swift
+++ Start/Minutes/Minutes/AppDelegate.swift
@@ -1,4 +1,11 @@
 import UIKit
+import AppCenter
+import AppCenterPush
+import AppCenterCrashes
+import AppCenterAnalytics
+import AppCenterDistribute
+
+// 485ec14e-8b44-419f-845e-3765acf7a34a
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate
@@ -8,6 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+
+        MSAppCenter.start("485ec14e-8b44-419f-845e-3765acf7a34a", withServices: [MSDistribute.self, MSCrashes.self, MSAnalytics.self, MSPush.self])
 
         return true
     }
